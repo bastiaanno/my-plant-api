ae2c42852fab0fde139ed7b895ad0bd2a087b557
Bastiaan Buitelaar <bastiaan@bnws.nl>
2026-06-07 23:56:00 +0200

    Update types

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,48 +1,55 @@
 export type User = {
-  id: string;
-  name: string;
-  email: string;
-  username: string;
-  isAdmin: boolean;
-  registrationDate: Date;
-};
-export type Activity = {
-  id: string;
-  title: string;
-  description: string;
-  venue: string;
-  startTime: Date;
-  endTime: Date;
-  signupDeadline: Date;
-  committee: string;
-  poster?: string;
-  totalSignUps: number;
-  userStatus: {
-    signedUp: boolean;
-    signupId?: string;
-    onWaitlist: boolean;
-    waitlistId?: string;
-  };
-};
-export type ActivitySignup = {
-  type: "signup" | "waitlist";
-  answers: {
-    [questionId: string]: string;
-  };
-};
-export type RemoveRegistration = {
-  activityId: string;
-};
-export type Wud = {
-  id: string;
-  message: string;
-  created: Date;
-  author: {
     id: string;
     name: string;
+    email: string;
     username: string;
-  };
+    isAdmin: boolean;
+    registrationDate: Date;
+};
+export type Activity = {
+    id: string;
+    title: string;
+    description: string;
+    venue: string;
+    startTime: Date;
+    endTime: Date;
+    signupDeadline: Date;
+    committee: string;
+    poster?: string;
+    totalSignUps: number;
+    userStatus: {
+        signedUp: boolean;
+        signupId?: string;
+        onWaitlist: boolean;
+        waitlistId?: string;
+    };
+    form: [
+        {
+            id: string;
+            question: string;
+            options?: string[];
+        }
+    ];
+};
+export type ActivitySignup = {
+    type: "signup" | "waitlist";
+    answers: {
+        [questionId: string]: string;
+    };
+};
+export type RemoveRegistration = {
+    activityId: string;
+};
+export type Wud = {
+    id: string;
+    message: string;
+    created: Date;
+    author: {
+        id: string;
+        name: string;
+        username: string;
+    };
 };
 export type PostWudjeRequest = {
-  message: string;
+    message: string;
 };
